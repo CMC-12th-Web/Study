328c7a73ec94c7b31420c6b588bc48d0b96b07e2
choiyeju <yeju1019@gmail.com>
2023-01-10 21:29:51 +0900

    카운터 만들기

--- src/screen/InitialSceen.js
+++ src/screen/InitialSceen.js
@@ -1,27 +1,35 @@
 import React, {useState} from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import Box from '../components/Box';
 
 const InitialScreen = () => {
-    const [show, setShow] = useState(true);
+    const [count, setCount] = useState(0);
 
   return (
     <>
       <SafeAreaProvider>
-        <Button
-            title="토글"
-            onPress={()=>{
-                if (show) setShow(false)
-                else setShow(true)
-            }}
-        ></Button>
-        {show&&
-            <Box rounded={true} size={'medium'} color={'blue'} />
-        }
+        <View style={styles.box}>
+            <Text style={styles.num}>{count}</Text>
+        </View>
+        <Button title='+1' onPress={()=>setCount(count+1)}></Button>
+        <Button title='-1' onPress={()=>setCount(count-1)}></Button>
       </SafeAreaProvider>
     </>
   );
 };
 
 export default InitialScreen;
+
+const styles = StyleSheet.create({
+    box: {
+        flex: 1,
+        
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    num: {
+        textAlign: 'center',
+        fontSize: 66,
+        fontWeight: '800',
+    },
+})
