6c145f67dafb0c084f9bbb945b258abd681b3b9d
gabi <70315572+Limgabi@users.noreply.github.com>
2023-01-11 18:43:59 +0900

    fix: border와 size 조정

--- 구현/1주차/림비/toggle/Box.js
+++ 구현/1주차/림비/toggle/Box.js
@@ -5,19 +5,19 @@ function Box({ rounded, size, color }) {
   return (
     <SafeAreaView>
       <View
-        style={styles(color).box}
+        style={styles(rounded, size, color).box}
       />
     </SafeAreaView>
   )
 }
 
-const styles = (color) => StyleSheet.create({
+const styles = (rounded, size, color) => StyleSheet.create({
   box: {
-    width: 100,
-    height: 100,
-    borderRadius: 15,
+    width: size === 'large' ? 128 : size === 'medium' ? 96 : 64,
+    height: size === 'large' ? 128 : size === 'medium' ? 96 : 64,
+    borderRadius: rounded ? 15 : 0,
     backgroundColor: color
-  },
+  }
 });
 
 export default Box
