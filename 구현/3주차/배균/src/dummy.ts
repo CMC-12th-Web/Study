1c654c9ec06b71ad902f71596fcbb2d54a359c3a
bgyoonseo <73841260+bgyoons@users.noreply.github.com>
2023-01-26 21:40:36 +0900

    3주차 구현

---
+++ 구현/3주차/배균/src/dummy.ts
@@ -0,0 +1,34 @@
+export const dummy = [
+  {
+    title: 'title',
+    star: '4.9',
+    price: '321771',
+    heart: true
+  },
+  {
+    title: 'title',
+    star: '4.9',
+    price: '321771',
+    heart: true
+  },
+  {
+    title: 'title',
+    star: '4.9',
+    price: '321771',
+    heart: true
+  },
+  {
+    title: 'title',
+    star: '4.9',
+    price: '321771',
+    heart: true
+  },
+  {
+    title: 'title',
+    star: '4.9',
+    price: '321771',
+    heart: true
+  },
+]
+
+export const category = ['공유오피스', '팝업스토어', '분위기좋은', '루프탑', '파티룸', '레저스포츠', '스터디룸', '반려동물', '스튜디오', '연습실', '공방']
